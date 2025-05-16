e4a176b160625e8343ad356fea87b196696be0ba
Haddaji-Maroia <maroia.haddaji@student.hepl.be>
2025-05-16 11:06:20 +0200

    Added stuff
    - change images folder to assets
    - added template-project page
    - some css mod

--- wp-content/themes/portfolio-maroia/functions.php
+++ wp-content/themes/portfolio-maroia/functions.php
@@ -3,22 +3,45 @@
 function portfoliomaroia_theme_support(){
     //Add Dynamic title tag support
     add_theme_support( 'title-tag' );
-
+    add_theme_support('custom-logo');
 }
 
 add_action( 'after_setup_theme', 'portfoliomaroia_theme_support' );
 
 
+function portfoliomaroia_menus(){
 
+    $locations = array(
+        'primary'=> 'Desktop main menu',
+        'footer'=> 'Footer main menu',
+    );
 
-
-function portfoliomaroia_register_styles() {
-    $version = wp_get_theme()->get( 'Version' ); //dynamic
-    wp_enqueue_style('portfolio-maroia-style', get_template_directory_uri() . '/style.css', array(), $version, 'all');
+    register_nav_menus( $locations );
 }
 
-add_action('wp_enqueue_scripts', 'portfoliomaroia_register_styles');
+add_action('init', 'portfoliomaroia_menus');
 
+
+
+//function portfoliomaroia_register_styles() {
+//    $version = wp_get_theme()->get( 'Version' ); //dynamic
+//    wp_enqueue_style('portfolio-maroia-style', get_template_directory_uri() . '/style.css', array(), $version, 'all');
+//}
+//
+//add_action('wp_enqueue_scripts', 'portfoliomaroia_register_styles');
+
+
+function portfolio_maroia_register_styles() {
+    wp_enqueue_style(
+        'portfolio-maroia-style',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        filemtime(get_template_directory() . '/style.css'),
+        'all'
+    );
+}
+
+add_action('wp_enqueue_scripts', 'portfolio_maroia_register_styles');
 
 
 
@@ -32,6 +55,13 @@ add_action('wp_enqueue_scripts', 'portfoliomaroia_register_scripts');
 
 
 ?>
+
+<?php
+
+add_filter('show_admin_bar', '__return_false');
+
+?>
+
 
 
 
