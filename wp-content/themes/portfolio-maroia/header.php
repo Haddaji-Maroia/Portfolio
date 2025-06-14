871e14c436ce2655b24a73de897f7a3231ad47fc
Haddaji-Maroia <maroia.haddaji@student.hepl.be>
2025-06-14 02:07:42 +0200

    new commit

--- wp-content/themes/portfolio-maroia/header.php
+++ wp-content/themes/portfolio-maroia/header.php
@@ -1,12 +1,11 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="author" content="Maroia Haddaji">
-    <meta name="keywords" content="portfolio, Maroia Haddaji, créative, développeuse">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="author" content="Maroia Haddaji" />
+    <meta name="keywords" content="portfolio, Maroia Haddaji, créative, développeuse" />
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -25,22 +24,98 @@
         }
         ?>
 
-        <!-- Burger Menu -->
-        <label for="menu-toggle" class="navbar__toggle">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
+        <!-- Checkbox toggle -->
+        <input type="checkbox" id="menu-toggle" class="menu-toggle" />
+
+        <!-- Burger menu -->
+        <label for="menu-toggle" class="navbar__toggle" aria-label="Toggle menu">
+            <span class="bar bar1"></span>
+            <span class="bar bar2"></span>
+            <span class="bar bar3"></span>
         </label>
 
-        <div class="container">
+        <!-- Contenuto visibile della pagina -->
+        <div class="page-content">
+            <div class="container">
+                <?php
+                wp_nav_menu([
+                    'theme_location' => 'primary',
+                    'menu_class' => 'menu',
+                    'container' => false,
+                    'fallback_cb' => false,
+                ]);
+                ?>
+
+                <?php if (function_exists('pll_the_languages')) : ?>
+                    <?php
+                    $languages = pll_the_languages([
+                        'raw' => 1,
+                        'hide_if_empty' => 0,
+                        'show_flags' => 0,
+                        'show_names' => 1
+                    ]);
+                    ?>
+                    <?php if (!empty($languages)) : ?>
+                        <div class="language-switcher">
+                            <button class="current-lang-btn" aria-haspopup="true" aria-expanded="false">
+                                <?php
+                                foreach ($languages as $lang) {
+                                    if ($lang['current_lang']) {
+                                        echo esc_html($lang['name']);
+                                        break;
+                                    }
+                                }
+                                ?>
+                                ⌄
+                            </button>
+                            <ul class="lang-dropdown">
+                                <?php foreach ($languages as $lang) : ?>
+                                    <?php if (!$lang['current_lang']) : ?>
+                                        <li>
+                                            <a href="<?= esc_url($lang['url']); ?>">
+                                                <?= esc_html($lang['name']); ?>
+                                            </a>
+                                        </li>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <!-- Off-canvas menu (mobile menu) -->
+        <div class="offcanvas-menu" aria-label="Menu mobile">
             <?php
             wp_nav_menu([
                 'theme_location' => 'primary',
-                'menu_class' => 'menu',
+                'menu_class' => 'offcanvas-menu-list',
                 'container' => false,
                 'fallback_cb' => false,
             ]);
             ?>
+
+            <?php if (function_exists('pll_the_languages')) : ?>
+                <?php if (!empty($languages)) : ?>
+                    <div class="offcanvas-language-switcher">
+                        <ul>
+                            <?php foreach ($languages as $lang) : ?>
+                                <li>
+                                    <a href="<?= esc_url($lang['url']); ?>">
+                                        <?= esc_html($lang['name']); ?>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
         </div>
+
     </nav>
 </header>
+
+<?php wp_footer(); ?>
+</body>
+</html>
